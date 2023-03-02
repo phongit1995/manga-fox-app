6681cc3926056cb7028256695d9347e8d0d5f5a5
peartalent <peartalent@gmail.com>
2023-03-02 14:30:00 +0700

    update config url

---
+++ lib/app_config.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+class AppConfig {
+  static const String androidId = "manga.fox.manga.reader.free";
+  static const String iosId = "com.example.mangaFoxApp";
+  static const String urlFacebook =
+      "https://www.facebook.com/groups/%20mangamanhwaanime";
+  static const String urlTerm = "https://manga-reader-6734b.firebaseapp.com/privacy-policy.html";
+  static const String urlStoreAndroid = "https://play.google.com/store/details?id=$androidId";
+  static const String urlStoreIos = "https://apps.apple.com/app/id$iosId";
+}
